8dcbf6d09148cf3ba57bf1151f7557e4f80d8dc3
anil <anil2anil@gmail.com>
2025-03-09 18:24:02 +0530

    Add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent, data: { title: 'Home - My App' } }, // Default route
-  // { path: 'home', component: AppComponent, data: { title: 'About Us - My App' } },
+  { path: '', component: HomeComponent, data: { title: 'Home' } }, // Default route
+  { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+  { path: 'about', component: AboutComponent, data: { title: 'About Us - My App' } },
   // { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown paths to home
 ];
 
